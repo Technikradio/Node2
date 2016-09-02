ca2f1a6727eb9f33c416e827c61e98e826c44a10
Doralitze <technikradio@icloud.com>
2016-09-02 10:00:14 +0200

    Fixed annoying UI bug with the expand bar.

--- src/core/org/technikradio/node/core/WorksheetBrowser.java
+++ src/core/org/technikradio/node/core/WorksheetBrowser.java
@@ -53,36 +53,41 @@ import org.technikradio.universal_tools.Console;
 import org.technikradio.universal_tools.Console.LogType;
 
 /**
- * This class is used to display a work sheet browser.
- * It stores its recent location inside APPDATA.
+ * This class is used to display a work sheet browser. It stores its recent
+ * location inside APPDATA.
+ * 
  * @author doralitze
  *
  */
 public final class WorksheetBrowser {
 
 	private final Window w;
+	private ExpandBar dataSourceBar;
+	private ExpandItem localExpandItem;
+	private ExpandItem remoteExpandItem;
 	private Composite localComposite;
 	private Composite remoteComposite;
 	private DataSource currentSelectedDS = null;
 	private boolean first = false;
-	
-	public WorksheetBrowser(){
+
+	public WorksheetBrowser() {
 		w = new Window("Worksheetbrowser");
 		w.getShell().addListener(SWT.Close, new Listener() {
 
 			@Override
 			public void handleEvent(Event arg0) {
-				if(!isFirst())
+				if (!isFirst())
 					return;
 				Console.log(LogType.StdOut, this, "Aborted worksheet selection. Closing app.");
 				Application.close();
-			}});
+			}
+		});
 		w.center();
 		{
-			
+
 			Label l1 = new Label(w.getContainer(WindowOrientation.BOTTOM), SWT.BORDER);
 			l1.setText("Bottom-Info");
-			
+
 		}
 		{
 			Button newButton = new Button(w.getContainer(WindowOrientation.TOP), SWT.PUSH);
@@ -91,111 +96,128 @@ public final class WorksheetBrowser {
 
 				@Override
 				public void handleEvent(Event arg0) {
-					if(currentSelectedDS != null)
-						currentSelectedDS.showResourceOpenDialog();
-				}});
+					if (getCurrentSelectedDS() != null) {
+						Console.log(LogType.StdOut, "org.technikradio.node.core.WorksheetBrowser.newButton",
+								"Clicked on NewButton. DS: " + getCurrentSelectedDS().getIdentifier());
+						getCurrentSelectedDS().showNewWorkFileDialog();
+					} else {
+						Console.log(LogType.StdOut, "org.technikradio.node.core.WorksheetBrowser.newButton",
+								"Clicked on NewButton. No data source.");
+					}
+				}
+			});
 		}
 		{
 			Button openButton = new Button(w.getContainer(WindowOrientation.TOP), SWT.PUSH);
 			openButton.setText("Open");
+			openButton.addListener(SWT.Selection, new Listener() {
+
+				@Override
+				public void handleEvent(Event arg0) {
+					if (getCurrentSelectedDS() != null) {
+						Console.log(LogType.StdOut, "org.technikradio.node.core.WorksheetBrowser.openButton",
+								"Clicked on OpenButton. DS: " + getCurrentSelectedDS().getIdentifier());
+						getCurrentSelectedDS().showResourceOpenDialog();
+					} else {
+						Console.log(LogType.StdOut, "org.technikradio.node.core.WorksheetBrowser.openButton",
+								"Clicked on OpenButton. No data source.");
+					}
+				}
+			});
 		}
 		{
-			ExpandBar dataSourceBar = new ExpandBar(w.getContainer(WindowOrientation.LEFT_TRAY), SWT.V_SCROLL);
-			localComposite = new Composite (dataSourceBar, SWT.NONE);
-			GridLayout layout = new GridLayout ();
+			dataSourceBar = new ExpandBar(w.getContainer(WindowOrientation.LEFT_TRAY), SWT.V_SCROLL);
+			localComposite = new Composite(dataSourceBar, SWT.NONE);
+			GridLayout layout = new GridLayout();
 			layout.marginLeft = layout.marginTop = layout.marginRight = layout.marginBottom = 10;
 			layout.verticalSpacing = 10;
 			localComposite.setLayout(layout);
-			ExpandItem localExpandItem = new ExpandItem (dataSourceBar, SWT.NONE, 0);
+			localExpandItem = new ExpandItem(dataSourceBar, SWT.NONE, 0);
 			localExpandItem.setText("Local data sources");
 			localExpandItem.setHeight(localComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
 			localExpandItem.setControl(localComposite);
 			localExpandItem.setExpanded(true);
-			//localExpandItem.setImage(image);
-			
-			remoteComposite = new Composite (dataSourceBar, SWT.NONE);
-			layout = new GridLayout (2, false);
+			// localExpandItem.setImage(image);
+
+			remoteComposite = new Composite(dataSourceBar, SWT.NONE);
+			layout = new GridLayout();
 			layout.marginLeft = layout.marginTop = layout.marginRight = layout.marginBottom = 10;
 			layout.verticalSpacing = 10;
+
 			remoteComposite.setLayout(layout);
-			ExpandItem remoteExpandItem = new ExpandItem (dataSourceBar, SWT.NONE, 1);
+			remoteExpandItem = new ExpandItem(dataSourceBar, SWT.NONE, 1);
 			remoteExpandItem.setText("Remote data sources");
 			remoteExpandItem.setHeight(localComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
 			remoteExpandItem.setControl(remoteComposite);
-			//localExpandItem.setImage(image);
-			
+			// localExpandItem.setImage(image);
+
 			dataSourceBar.setSpacing(8);
 		}
+		fillDSS();
 		w.open();
 		w.setSize(750, 400);
-		fillDSS();
+		
 	}
 
 	private void fillDSS() {
 		int numLoc = 0;
 		int numRem = 0;
 		boolean first = true;
-		for(DataSource ds : PluginRegistry.getAllRegisteredDataSources()){
-			if(first && !ds.isRemoteDataSource()){
+		for (DataSource ds : PluginRegistry.getAllRegisteredDataSources()) {
+			if (first && !ds.isRemoteDataSource()) {
 				first = false;
-				currentSelectedDS = ds;
+				setCurrentSelectedDS(ds);
 			}
 			Console.log(LogType.Information, this, "Adding data source: " + ds.getIdentifier() + "; " + ds.getName());
-			if(ds.isRemoteDataSource()){
+
+			Composite c = null;
+			int ci;
+			if (ds.isRemoteDataSource()) {
 				numRem++;
-				Composite c = new Composite(remoteComposite, SWT.NONE);
-				GridLayout layout = new GridLayout (2, false);
-				layout.marginLeft = layout.marginTop = layout.marginRight = layout.marginBottom = 10;
-				layout.verticalSpacing = 10;
-				c.setLayout(new FillLayout());
-				c.setBackground(Colors.BLACK);
-				if(numRem % 2 == 0){
-					c.setBackground(Colors.GRAY_DESIGN.getTextColor());
-				}
-				Label l = new Label(c, SWT.BORDER);
-				l.setText(ds.getName());
-				Label l1 = new Label(w.getContainer(WindowOrientation.BOTTOM), SWT.BORDER);
-				l1.setText(ds.getName());
-
-				final DataSource d = ds;
-				c.addListener(SWT.MouseDown, new Listener(){
-
-					@Override
-					public void handleEvent(Event arg0) {
-						Console.log(LogType.StdOut, this, "Clicked. " + d.getIdentifier());
-						currentSelectedDS = d;
-					}
-					
-				});
+				c = new Composite(remoteComposite, SWT.NONE);
+				ci = numRem;
 			} else {
 				numLoc++;
-				Composite c = new Composite(localComposite, SWT.NONE);
-				GridLayout layout = new GridLayout (2, false);
-				layout.marginLeft = layout.marginTop = layout.marginRight = layout.marginBottom = 10;
-				layout.verticalSpacing = 10;
-				c.setLayout(new FillLayout());
-				c.setBackground(Colors.BLACK);
-				if(numLoc % 2 == 0){
-					c.setBackground(Colors.GRAY_DESIGN.getTextColor());
-				}
-				Label l = new Label(c, SWT.BORDER);
-				l.setText(ds.getName());
-				final DataSource d = ds;
-				c.addListener(SWT.MouseDown, new Listener(){
-
-					@Override
-					public void handleEvent(Event arg0) {
-						Console.log(LogType.StdOut, this, "Clicked. " + d.getIdentifier());
-						currentSelectedDS = d;
-					}
-					
-				});
+				c = new Composite(localComposite, SWT.NONE);
+				ci = numLoc;
 			}
+
+			GridLayout layout = new GridLayout(2, false);
+			layout.marginLeft = layout.marginTop = layout.marginRight = layout.marginBottom = 10;
+			layout.verticalSpacing = 10;
+			c.setLayout(new FillLayout());
+			
+			if (ci % 2 == 0) {
+				c.setBackground(Colors.GRAY_DESIGN.getTextColor());
+			}
+			{
+				//GridLayout l = (GridLayout) remoteComposite.getLayout();
+				//l.numColumns++;
+			}
+			Label l = new Label(c, SWT.BORDER);
+			l.setText(ds.getName());
+			
+			
+			final DataSource d = ds;
+			c.addListener(SWT.MouseDown, new Listener() {
+
+				@Override
+				public void handleEvent(Event arg0) {
+					Console.log(LogType.StdOut, this, "Clicked. " + d.getIdentifier());
+					setCurrentSelectedDS(d);
+				}
+
+			});
 		}
+		remoteExpandItem.setHeight(remoteComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		localExpandItem.setHeight(localComposite.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		
 	}
 
 	/**
-	 * Use this method in order to check if this dialog is displayed at the launch of node.
+	 * Use this method in order to check if this dialog is displayed at the
+	 * launch of node.
+	 * 
 	 * @return the first flag
 	 */
 	public boolean isFirst() {
@@ -203,10 +225,32 @@ public final class WorksheetBrowser {
 	}
 
 	/**
-	 * Set the first flag to true if this instance is displayed at the start of node.
-	 * @param first the first flag to set
+	 * Set the first flag to true if this instance is displayed at the start of
+	 * node.
+	 * 
+	 * @param first
+	 *            the first flag to set
 	 */
 	public void setFirst(boolean first) {
 		this.first = first;
+	}
+
+	/**
+	 * Use this method in order to get the current selected data source.
+	 * 
+	 * @return the current selected data source
+	 */
+	public DataSource getCurrentSelectedDS() {
+		return currentSelectedDS;
+	}
+
+	/**
+	 * Use this method to set the current selected data source.
+	 * 
+	 * @param currentSelectedDS
+	 *            the current selected data source to set
+	 */
+	public void setCurrentSelectedDS(DataSource currentSelectedDS) {
+		this.currentSelectedDS = currentSelectedDS;
 	}
 }
